d7517783f41efa18ae89ee406fe0ecd126f3e66f
pavan <pavanoltraining@gmail.com>
2022-07-04 08:05:36 +0530

    comming 2nd time

--- MySampleProject/src/pack1/Test/MyClass.java
+++ MySampleProject/src/pack1/Test/MyClass.java
@@ -2,6 +2,9 @@ package pack1.Test;
 
 public class MyClass {
 
+	public static void main(String args[])
+	{
+		System.out.println("testionmg....");
+	}
 	
-	//comment
 }
---
+++ MySampleProject/src/pack1/Test/MyClass2.java
@@ -0,0 +1,5 @@
+package pack1.Test;
+
+public class MyClass2 {
+
+}
